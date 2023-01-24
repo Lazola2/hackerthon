7fa51f947d5cb1f4832f1cd50a6c8ddc5d6556d4
Lazola2 <lazolamakubalo271@gmail.com>
2023-01-24 14:39:04 +0200

    Updates

--- code/app.js
+++ code/app.js
@@ -3,15 +3,15 @@ const debts = [
     {
         id: 1,
         partyOwed: 'Nedbank loan',
-        price: 36000,
-        balance: 28000,
+        price: 66000,
+        balance: 34000,
         description: 'School fees loan',
         paymentDate: '22-04-2023'
     },
     {
         id: 2,
         partyOwed: 'Clothing Account',
-        price: 29000,
+        price: 30000,
         balance: 3000,
         description: 'Sport Scene account',
         paymentDate: '22-04-2023'
@@ -19,7 +19,7 @@ const debts = [
     {
         id: 3,
         partyOwed: 'Entertainment',
-        price: 5000,
+        price: 15000,
         balance: 2470,
         description: 'Money for groove',
         paymentDate: '22-04-2023'
@@ -51,13 +51,14 @@ username.innerHTML = selected.name;
 let debtPercentages = [];
 
 debts.forEach(debt => {
-    let percentage = Math.round(((debt.price - debt.balance)/ debt.price) * 100);
+    let percentage = Math.round((debt.balance/ debt.price) * 100);
     debtPercentages.push(percentage);
     
     let debtItem = `
         <div class="debt-item">
             <div class="progress-bar-section">
-                <div class="progress-bar">
+                <div class="progress-bar" style="
+                    background: conic-gradient( var(--text-theme) calc(360deg * .${percentage}), rgba(255, 255, 255, 0.181) 0deg );">
                     <div class="inner-circle">
                         <p class="debt-remaining-percentage">
                             ${percentage}%
@@ -78,10 +79,44 @@ debts.forEach(debt => {
     debtsContainer.innerHTML += debtItem;
 });
 
+// get the small progress bar
+
 // update the average
 let totalPercentages = debtPercentages.reduce((a,b) => a + b, 0);
 let average = totalPercentages / debtPercentages.length;
 
+
 // get overall debt container
+const outerCircle = document.querySelector('.overall-debt');
+outerCircle.style.background = `
+    conic-gradient( var(--text-theme) 
+    calc(360deg * .${average < 10 ? '0'+average: average}), 
+    rgba(255, 255, 255, 0.181) 0deg )`;
+
 const overallDebtContainer = document.querySelector('.overall-debt>.inner-circle');
 overallDebtContainer.innerHTML += `<p class="overall-percentage">${Math.round(average)}%</p>`;
+
+// load the debts in the debts setting container
+
+    const debtSettingsContainer = document.querySelector('.debt-item-settings-container');
+    
+    debts.forEach(debt => {
+        debtSettingsContainer.innerHTML += 
+            `<div class="debt-item-settings overflow-hidden">
+                <div class="upper d-flex px-3 justify-content-between align-items-center">
+                    <p class="item-name pt-3">${debt.partyOwed}</p>
+                    <i class="bi bi-x-square-fill cancel-icon"
+                        onclick="handleCancelDebt(${debt.id})">
+                    </i>
+                </div>
+                <div class="lower d-flex justify-content-between">
+                    <input type="number" class="form-control price-input" placeholder="amount in rands">
+                    <button class="pay-button">Pay</button>
+                </div>
+            </div>` 
+    });
+
+
+
+
+
--- code/controls.js
+++ code/controls.js
@@ -1,8 +1,15 @@
+// salutation 
+const salutationLabel = document.querySelector('.salutation');
 
+// settings button
 const settings = document.querySelector('.settings');
+
+// toggling the settings page into view
 const settingsPage = document.querySelector('.settings-page');
 settings.addEventListener('click', () => {
     settingsPage.classList.toggle('active-page');
-    settings.classList.toggle('active')
-
+    settings.classList.toggle('active');
+    salutationLabel.classList.toggle('invisible');
 });
+
+
